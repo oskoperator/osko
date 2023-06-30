37bd39a71f656f3385910a0e8632919570d861d6
Robin Opletal <me@robinopletal.com>
2023-06-30 16:30:59 +0200

    feat(operator): kubebuilder kick-off
    Signed-off-by: Robin Opletal <me@robinopletal.com>

---
+++ api/v1/datasource_types.go
@@ -0,0 +1,49 @@
+package v1
+
+import (
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+)
+
+// EDIT THIS FILE!  THIS IS SCAFFOLDING FOR YOU TO OWN!
+// NOTE: json tags are required.  Any new fields you add must have json tags for the fields to be serialized.
+
+// DatasourceSpec defines the desired state of Datasource
+type DatasourceSpec struct {
+	// INSERT ADDITIONAL SPEC FIELDS - desired state of cluster
+	// Important: Run "make" to regenerate code after modifying this file
+
+	// Foo is an example field of Datasource. Edit datasource_types.go to remove/update
+	Foo string `json:"foo,omitempty"`
+}
+
+// DatasourceStatus defines the observed state of Datasource
+type DatasourceStatus struct {
+	// INSERT ADDITIONAL STATUS FIELD - define observed state of cluster
+	// Important: Run "make" to regenerate code after modifying this file
+}
+
+//+kubebuilder:object:root=true
+//+kubebuilder:subresource:status
+//+kubebuilder:resource:scope=Cluster
+
+// Datasource is the Schema for the datasources API
+type Datasource struct {
+	metav1.TypeMeta   `json:",inline"`
+	metav1.ObjectMeta `json:"metadata,omitempty"`
+
+	Spec   DatasourceSpec   `json:"spec,omitempty"`
+	Status DatasourceStatus `json:"status,omitempty"`
+}
+
+//+kubebuilder:object:root=true
+
+// DatasourceList contains a list of Datasource
+type DatasourceList struct {
+	metav1.TypeMeta `json:",inline"`
+	metav1.ListMeta `json:"metadata,omitempty"`
+	Items           []Datasource `json:"items"`
+}
+
+func init() {
+	SchemeBuilder.Register(&Datasource{}, &DatasourceList{})
+}

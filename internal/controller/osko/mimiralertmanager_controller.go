56ccb2d186b850da72f6dc1a69db12a2ccf3e4e0
Hy3n4 <hy3nk4@gmail.com>
2023-12-20 12:49:01 +0100

    feat(alert): More alerting features
    - mainly (for now) hardcoded OpsGenie support
    
    Signed-off-by: Hy3n4 <hy3nk4@gmail.com>

--- internal/controller/osko/mimiralertmanager_controller.go
+++ internal/controller/osko/mimiralertmanager_controller.go
@@ -2,20 +2,28 @@ package osko
 
 import (
 	"context"
+	"github.com/go-logr/logr"
+	mimirclient "github.com/grafana/mimir/pkg/mimirtool/client"
+	monitoringv1alpha1 "github.com/prometheus-operator/prometheus-operator/pkg/apis/monitoring/v1alpha1"
+	apierrors "k8s.io/apimachinery/pkg/api/errors"
 
+	oskov1alpha1 "github.com/oskoperator/osko/api/osko/v1alpha1"
 	"k8s.io/apimachinery/pkg/runtime"
 	ctrl "sigs.k8s.io/controller-runtime"
 	"sigs.k8s.io/controller-runtime/pkg/client"
-	"sigs.k8s.io/controller-runtime/pkg/log"
-
-	oskov1alpha1 "github.com/oskoperator/osko/api/osko/v1alpha1"
+	ctrllog "sigs.k8s.io/controller-runtime/pkg/log"
 )
 
 // MimirAlertManagerReconciler reconciles a MimirAlertManager object
 type MimirAlertManagerReconciler struct {
 	client.Client
-	Scheme *runtime.Scheme
+	Scheme      *runtime.Scheme
+	MimirClient *mimirclient.MimirClient
 }
+
+const (
+	errGetMAM = "Failed to get MimirAlertManager"
+)
 
 //+kubebuilder:rbac:groups=osko.openslo,resources=mimiralertmanagers,verbs=get;list;watch;create;update;patch;delete
 //+kubebuilder:rbac:groups=osko.openslo,resources=mimiralertmanagers/status,verbs=get;update;patch
@@ -31,11 +39,31 @@ type MimirAlertManagerReconciler struct {
 // For more details, check Reconcile and its Result here:
 // - https://pkg.go.dev/sigs.k8s.io/controller-runtime@v0.16.3/pkg/reconcile
 func (r *MimirAlertManagerReconciler) Reconcile(ctx context.Context, req ctrl.Request) (ctrl.Result, error) {
-	_ = log.FromContext(ctx)
+	log := ctrllog.FromContext(ctx)
 
-	// TODO(user): your logic here
+	mimirAlertManager := &oskov1alpha1.MimirAlertManager{}
+
+	err := r.Get(ctx, req.NamespacedName, mimirAlertManager)
+	if err != nil {
+		if apierrors.IsNotFound(err) {
+			log.V(1).Info("MimirAlertManager resource not found. Object must have been deleted.")
+			return ctrl.Result{}, nil
+		}
+		log.Error(err, errGetMAM)
+		return ctrl.Result{}, err
+	}
 
 	return ctrl.Result{}, nil
+}
+
+func (r *MimirAlertManagerReconciler) createMimirAlertManager(log logr.Logger, alertmanagerConfig *monitoringv1alpha1.AlertmanagerConfig) error {
+	log.Info("Creating MimirAlertManager")
+
+	if err := r.MimirClient.CreateAlertmanagerConfig(context.Background(), alertmanagerConfig.Name, nil); err != nil {
+		log.Error(err, "Failed to create MimirAlertManager")
+		return err
+	}
+	return nil
 }
 
 // SetupWithManager sets up the controller with the Manager.
--- internal/helpers/alertmanager_helper.go
+++ internal/helpers/alertmanager_helper.go
@@ -3,14 +3,24 @@ package helpers
 import (
 	openslov1 "github.com/oskoperator/osko/api/openslo/v1"
 	monitoringv1alpha1 "github.com/prometheus-operator/prometheus-operator/pkg/apis/monitoring/v1alpha1"
+	apiextensionsv1 "k8s.io/apiextensions-apiserver/pkg/apis/apiextensions/v1"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"k8s.io/apimachinery/pkg/util/json"
 )
 
 func CreateAlertManagerConfig(alertNotificationTarget *openslov1.AlertNotificationTarget) (*monitoringv1alpha1.AlertmanagerConfig, error) {
+	// TODO: Add support for other notification targets as for now we only support OpsGenie and it is hardcoded just for testing purposes
+	// TODO: Current OpenSLO specification only supports `string` as the type for target, so we would have to create a new CRD for each notification target
+	var (
+		receivers  []monitoringv1alpha1.Receiver
+		routesJSON []apiextensionsv1.JSON
+	)
+
 	typeMeta := metav1.TypeMeta{
 		Kind:       "AlertmanagerConfig",
 		APIVersion: "monitoring.coreos.com/v1alpha1",
 	}
+
 	objectMeta := metav1.ObjectMeta{
 		Name:      alertNotificationTarget.Name,
 		Namespace: alertNotificationTarget.Namespace,
@@ -22,17 +32,85 @@ func CreateAlertManagerConfig(alertNotificationTarget *openslov1.AlertNotificati
 		Priority: alertNotificationTarget.Spec.Target.OpsGenie.Priority,
 	}
 
-	receiver := monitoringv1alpha1.Receiver{
-		Name: "OpeGenie",
+	opsGenieReceiver := monitoringv1alpha1.Receiver{
+		Name: "OpsGenie",
 		OpsGenieConfigs: []monitoringv1alpha1.OpsGenieConfig{
 			opsgenieConfig,
 		},
 	}
 
-	alertManagerConfigSpec := monitoringv1alpha1.AlertmanagerConfigSpec{
-		Receivers: []monitoringv1alpha1.Receiver{
-			receiver,
+	receivers = append(receivers, opsGenieReceiver)
+
+	alertRoutes := []monitoringv1alpha1.Route{
+		{
+			Receiver: "heartbeat",
+			Matchers: []monitoringv1alpha1.Matcher{
+				{
+					Name:  "alertname",
+					Value: "DeadMansSwitch",
+				},
+			},
+			GroupInterval:  "1m",
+			RepeatInterval: "1m",
+			Continue:       true,
+		}, {
+			Receiver: "heartbeat_betteruptime",
+			Matchers: []monitoringv1alpha1.Matcher{
+				{
+					Name:  "alertname",
+					Value: "DeadMansSwitch",
+				},
+			},
+			GroupInterval:  "1m",
+			RepeatInterval: "1m",
+		}, {
+			Receiver: "betteruptime",
+			Matchers: []monitoringv1alpha1.Matcher{
+				{
+					Name:  "alertname",
+					Value: ".*",
+					Regex: true,
+				},
+			},
+			Continue: true,
+		}, {
+			Receiver: "opsgenie",
+			Matchers: []monitoringv1alpha1.Matcher{
+				{
+					Name:  "alertname",
+					Value: ".*",
+					Regex: true,
+				},
+			},
 		},
+	}
+
+	alerRoutesJSON, err := json.Marshal(alertRoutes)
+	if err != nil {
+		return nil, err
+	}
+
+	route := apiextensionsv1.JSON{Raw: alerRoutesJSON}
+	routesJSON = append(routesJSON, route)
+
+	routes := &monitoringv1alpha1.Route{
+		Receiver: opsGenieReceiver.Name,
+		GroupBy: []string{
+			"alertname",
+			"job",
+			"statefulset",
+			"daemonset",
+		},
+		Continue:       true,
+		GroupWait:      "30s",
+		GroupInterval:  "5min",
+		RepeatInterval: "12h",
+		Routes:         routesJSON,
+	}
+
+	alertManagerConfigSpec := monitoringv1alpha1.AlertmanagerConfigSpec{
+		Route:     routes,
+		Receivers: receivers,
 	}
 
 	alertManagerConfig := &monitoringv1alpha1.AlertmanagerConfig{

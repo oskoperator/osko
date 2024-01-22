6981fd1d9a381d1cad7d0c85105ec838ffe8f0e2
Robin Opletal <me@robinopletal.com>
2024-01-22 13:53:52 +0100

    chore: clean up redundant todos
    Signed-off-by: Robin Opletal <me@robinopletal.com>

--- internal/controller/monitoring.coreos.com/prometheusrule_controller.go
+++ internal/controller/monitoring.coreos.com/prometheusrule_controller.go
@@ -36,15 +36,6 @@ type PrometheusRuleReconciler struct {
 // +kubebuilder:rbac:groups=monitoring.coreos.com,resources=prometheusrules/finalizers,verbs=update
 // +kubebuilder:rbac:groups=core,resources=events,verbs=create;patch
 
-// Reconcile is part of the main kubernetes reconciliation loop which aims to
-// move the current state of the cluster closer to the desired state.
-// TODO(user): Modify the Reconcile function to compare the state specified by
-// the PrometheusRule object against the actual cluster state, and then
-// perform operations to make the cluster state reflect the state specified by
-// the user.
-//
-// For more details, check Reconcile and its Result here:
-// - https://pkg.go.dev/sigs.k8s.io/controller-runtime@v0.16.0/pkg/reconcile
 func (r *PrometheusRuleReconciler) Reconcile(ctx context.Context, req ctrl.Request) (ctrl.Result, error) {
 	log := ctrllog.FromContext(ctx)
 
--- internal/controller/openslo/alertcondition_controller.go
+++ internal/controller/openslo/alertcondition_controller.go
@@ -21,19 +21,8 @@ type AlertConditionReconciler struct {
 //+kubebuilder:rbac:groups=openslo.com,resources=alertconditions/status,verbs=get;update;patch
 //+kubebuilder:rbac:groups=openslo.com,resources=alertconditions/finalizers,verbs=update
 
-// Reconcile is part of the main kubernetes reconciliation loop which aims to
-// move the current state of the cluster closer to the desired state.
-// TODO(user): Modify the Reconcile function to compare the state specified by
-// the AlertCondition object against the actual cluster state, and then
-// perform operations to make the cluster state reflect the state specified by
-// the user.
-//
-// For more details, check Reconcile and its Result here:
-// - https://pkg.go.dev/sigs.k8s.io/controller-runtime@v0.15.0/pkg/reconcile
 func (r *AlertConditionReconciler) Reconcile(ctx context.Context, req ctrl.Request) (ctrl.Result, error) {
 	_ = log.FromContext(ctx)
-
-	// TODO(user): your logic here
 
 	return ctrl.Result{}, nil
 }

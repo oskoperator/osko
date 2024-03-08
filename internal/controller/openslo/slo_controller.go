f3fbb985559ebe8f8829c0be40978e47e82c1090
Robin Opletal <49439044+fourstepper@users.noreply.github.com>
2024-03-08 22:31:17 +0100

    Fix PrometheusRule reconcile on SLO change (#72)
    * fix(prometheusrule): fix prometheusrule reconcile
    
    - added back to `main.go`
    - tried to use `r.Update`, but it does not seem to work.
    
    Signed-off-by: Hy3n4 <hy3nk4@gmail.com>
    
    * feat(prom): reconcile promrule based on slo change
    
    Signed-off-by: Hy3n4 <hy3nk4@gmail.com>
    
    * last few adjustments b4 merge
    
    Signed-off-by: Robin Opletal <me@robinopletal.com>
    
    ---------
    
    Signed-off-by: Hy3n4 <hy3nk4@gmail.com>
    Signed-off-by: Robin Opletal <me@robinopletal.com>
    Co-authored-by: Hy3n4 <hy3nk4@gmail.com>

--- internal/controller/openslo/slo_controller.go
+++ internal/controller/openslo/slo_controller.go
@@ -3,7 +3,6 @@ package controller
 import (
 	"context"
 	"fmt"
-
 	openslov1 "github.com/oskoperator/osko/api/openslo/v1"
 	oskov1alpha1 "github.com/oskoperator/osko/api/osko/v1alpha1"
 	"github.com/oskoperator/osko/internal/helpers"
@@ -102,15 +101,15 @@ func (r *SLOReconciler) Reconcile(ctx context.Context, req ctrl.Request) (ctrl.R
 		return ctrl.Result{}, err
 	}
 
-	promRule := &monitoringv1.PrometheusRule{}
+	prometheusRule := &monitoringv1.PrometheusRule{}
 	err = r.Get(ctx, types.NamespacedName{
 		Name:      slo.Name,
 		Namespace: slo.Namespace,
-	}, promRule)
+	}, prometheusRule)
 
 	if apierrors.IsNotFound(err) {
 		log.Info("PrometheusRule not found. Let's make one.")
-		promRule, err = helpers.CreatePrometheusRule(slo, sli)
+		prometheusRule, err = helpers.CreatePrometheusRule(slo, sli)
 		if err != nil {
 			err = utils.UpdateStatus(ctx, slo, r.Client, "Ready", metav1.ConditionFalse, "Failed to create Prometheus Rule")
 			if err != nil {
@@ -120,9 +119,9 @@ func (r *SLOReconciler) Reconcile(ctx context.Context, req ctrl.Request) (ctrl.R
 			log.Error(err, "Failed to create new PrometheusRule")
 			return ctrl.Result{}, err
 		}
-		if err := r.Create(ctx, promRule); err != nil {
+		if err := r.Create(ctx, prometheusRule); err != nil {
 			r.Recorder.Event(slo, "Error", "FailedToCreatePrometheusRule", "Failed to create Prometheus Rule")
-			if err := r.Status().Update(ctx, promRule); err != nil {
+			if err := r.Status().Update(ctx, prometheusRule); err != nil {
 				log.Error(err, "Failed to update SLO status")
 				if err = utils.UpdateStatus(ctx, slo, r.Client, "Ready", metav1.ConditionFalse, "Failed to create Prometheus Rule"); err != nil {
 					log.Error(err, "Failed to update SLO ready status")
@@ -142,6 +141,8 @@ func (r *SLOReconciler) Reconcile(ctx context.Context, req ctrl.Request) (ctrl.R
 		}
 	}
 
+	log.V(1).Info("PrometheusRule found", "Name", prometheusRule.Name, "Namespace", prometheusRule.Namespace)
+
 	mimirRule := &oskov1alpha1.MimirRule{}
 	err = r.Get(ctx, types.NamespacedName{
 		Name:      slo.Name,
@@ -150,7 +151,7 @@ func (r *SLOReconciler) Reconcile(ctx context.Context, req ctrl.Request) (ctrl.R
 
 	if apierrors.IsNotFound(err) {
 		log.Info("MimirRule not found. Let's make one.")
-		mimirRule, err = helpers.NewMimirRule(slo, promRule, &ds.Spec.ConnectionDetails)
+		mimirRule, err = helpers.NewMimirRule(slo, prometheusRule, &ds.Spec.ConnectionDetails)
 		if err != nil {
 			if err = utils.UpdateStatus(ctx, slo, r.Client, "Ready", metav1.ConditionFalse, "Failed to create Mimir Rule Object"); err != nil {
 				log.Error(err, "Failed to update SLO status")
@@ -188,9 +189,14 @@ func (r *SLOReconciler) Reconcile(ctx context.Context, req ctrl.Request) (ctrl.R
 		}
 	}
 
+	log.V(1).Info("MimirRule found", "Name", mimirRule.Name, "Namespace", mimirRule.Namespace)
+
 	if err = utils.UpdateStatus(ctx, slo, r.Client, "Ready", metav1.ConditionTrue, "PrometheusRule created"); err != nil {
+		log.V(1).Error(err, "Failed to update SLO status")
 		return ctrl.Result{}, err
 	}
+
+	log.V(1).Info("Reconciliation completed")
 
 	return ctrl.Result{}, nil
 }
@@ -216,7 +222,7 @@ func (r *SLOReconciler) findObjectsForSli() func(ctx context.Context, a client.O
 			FieldSelector: fields.OneTermEqualSelector(indicatorRef, a.GetName()),
 			Namespace:     a.GetNamespace(),
 		}
-		err := r.List(ctx, attachedSLOs, listOpts)
+		err := r.Client.List(ctx, attachedSLOs, listOpts)
 		if err != nil {
 			return []reconcile.Request{}
 		}

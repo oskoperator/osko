cd22d793a9f52c4a71195f4ac5d72ca51e98ddab
Robin Opletal <49439044+fourstepper@users.noreply.github.com>
2024-03-14 10:53:56 +0100

    fix(sli_measurement): clamp_max to 1 (#79)
    * fix(sli_measurement): clamp_max to 1
    
    Signed-off-by: Robin Opletal <me@robinopletal.com>
    
    * Update internal/helpers/prometheus_helper.go
    
    Co-authored-by: David Jetelina <6594163+djetelina@users.noreply.github.com>
    Signed-off-by: Robin Opletal <49439044+fourstepper@users.noreply.github.com>
    
    ---------
    
    Signed-off-by: Robin Opletal <me@robinopletal.com>
    Signed-off-by: Robin Opletal <49439044+fourstepper@users.noreply.github.com>
    Co-authored-by: David Jetelina <6594163+djetelina@users.noreply.github.com>

--- internal/helpers/prometheus_helper.go
+++ internal/helpers/prometheus_helper.go
@@ -118,7 +118,7 @@ func (mrs *MonitoringRuleSet) createSliMeasurementRecordingRule(totalRule, goodR
 	totalLabels := mapToColonSeparatedString(totalRule.Labels)
 	return monitoringv1.Rule{
 		Record: fmt.Sprintf("%s_sli_measurement", RecordPrefix),
-		Expr:   intstr.FromString(fmt.Sprintf("%s{%s} / %s{%s}", goodRule.Record, goodLabels, totalRule.Record, totalLabels)),
+		Expr:   intstr.FromString(fmt.Sprintf("clamp_max(%s{%s} / %s{%s}, 1)", goodRule.Record, goodLabels, totalRule.Record, totalLabels)),
 		Labels: map[string]string{
 			"service":  mrs.Slo.Spec.Service,
 			"sli_name": mrs.Sli.Name,

7054515cdc1ccd1bfb73e047802c0e29d80d6a33
Hy3n4 <hy3nk4@gmail.com>
2024-06-24 12:30:16 +0200

    feat(alertmanagerconfig): add success event
    Signed-off-by: Hy3n4 <hy3nk4@gmail.com>

--- internal/controller/osko/alertmanagerconfig_controller.go
+++ internal/controller/osko/alertmanagerconfig_controller.go
@@ -125,6 +125,7 @@ func (r *AlertManagerConfigReconciler) Reconcile(ctx context.Context, req ctrl.R
 		return ctrl.Result{}, err
 	}
 
+	r.Recorder.Event(amc, "Normal", "AlertManagerConfigCreated", "AlertManagerConfig created successfully")
 	if err = utils.UpdateStatus(ctx, amc, r.Client, "Ready", metav1.ConditionTrue, "PrometheusRule created"); err != nil {
 		log.V(1).Error(err, "Failed to update SLO status")
 		return ctrl.Result{}, err
